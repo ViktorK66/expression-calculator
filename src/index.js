a92f2d80092a44ff7641790b14e4dbb0557a1a43
ViktorK66 <55109336+ViktorK66@users.noreply.github.com>
2019-10-18 23:11:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,43 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  let brackets = [];
+  let arraySymbols = expr.match(/\d+|\+|\-|\(|\)|\*|\//g);
+  function calculate(expr) {
+    for (let i = 0; i < expr.length; i++) {
+      if (expr[i] == '*' || expr[i] == '/') {
+        if (expr[i] == '*') expr[i-1] = Number(expr[i-1]) * Number(expr[i+1]);
+        else {
+          if (Number(expr[i+1]) == 0) throw TypeError('TypeError: Division by zero.');
+          expr[i-1] = Number(expr[i-1]) / Number(expr[i+1]);
+        }
+        expr.splice(i, 2);
+        i--;
+      }
+    }
+    for (let i = 0; i < expr.length; i++) {
+      if (expr[i] == '+' || expr[i] == '-') {
+        if (expr[i] == '+')  expr[i-1] = Number(expr[i-1]) + Number(expr[i+1]);
+        else expr[i-1] = Number(expr[i-1]) - Number(expr[i+1]);
+        expr.splice(i, 2);
+        i--;
+      }
+    }
+    return expr[0];
+  }
+  for (let i = 0; i < arraySymbols.length; i++) {
+    if (arraySymbols[i] == '(') brackets.push(i);
+    else if (arraySymbols[i] == ')') {
+      let start = brackets.pop();
+      if (start == undefined) throw Error('ExpressionError: Brackets must be paired');
+      let length = i - start - 1;
+      arraySymbols.splice(i, 1);
+      arraySymbols[start] = calculate(arraySymbols.splice(start + 1, length));
+      i = i - length - 1;
+    }
+  }
+  if (brackets.length) throw Error('ExpressionError: Brackets must be paired');
+  return calculate(arraySymbols);
 }
 
 module.exports = {
